1b9a6de192dbe2095191b3bd2f68ed0aec5eef1b
sbot <sbot.opensipa@gmail.com>
2015-09-29 20:06:59 +0200

    Code purge

--- theme/verification.php
+++ theme/verification.php
@@ -6,8 +6,8 @@ if (empty($_SESSION['username'])) {
 	echo'<div id="content" class="clearfix">';
 		echo'<div class="content-row">';
 			echo '<h2><p><a href="index.php">Fai il Login.</a></p></h2>';
-	echo '</div>';			
-            echo '</div>';
+                echo '</div>';			
+        echo '</div>';
 	die();
 }
 ?>
