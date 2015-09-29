eac02627ff7b08fe35e609f3a3242e520056739d
sbot <sbot.opensipa@gmail.com>
2015-09-29 23:59:46 +0200

    new login

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php include 'theme/header.php'; ?>
 
-	<div id="content" class="clearfix">
-		<div class="content-row">
-				<h2>Per accedere alla piattaforma di gestione del Bot devi fare il Login:</h2>
-				   <form id='login' action='login.php' method='post' accept-charset='UTF-8'>
-					  <fieldset >
-					  <legend>Login</legend>
-					  <input type='hidden' name='submitted' id='submitted' value='1'/>
-					  <label for='username' >UserName*:</label>
-					  <input type='text' name='username' id='username'  maxlength="50" />
-					  <label for='password' >Password*:</label>
-					  <input type='password' name='password' id='password' maxlength="50" />
-					  <input type='submit' name='Submit' value='Submit' />
-					  </fieldset>
-				</form>
 
+	<div id="content" class="clearfix">
+            <div class="content-row">
+		<h2>Per accedere alla piattaforma di gestione di {S}bot devi fare il Login:</h2>        
+                <form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+		<fieldset >
+		<legend>Login</legend>
+		<input type='hidden' name='submitted' id='submitted' value='1'/>
+                <div class="form-group">
+                <label for='username' >UserName:</label>
+                <input type='text'  class="form-control" name='username' id='username'  placeholder="Inserisci lo username" maxlength="50" />
+                </div>
+                <br>
+                <div class="form-group">
+		<label for='password' >Password: </label>
+		<input type='password' class="form-control" name='password' id='password' placeholder="Inserisci la password" maxlength="50" />
+		</div>
+                <br>
+		<input type='submit' name='Submit' value='Submit' />
+		</fieldset>
+		</form>
 		</div>
 	</div>
+
 <!-- Footer page -->
 <?php include 'theme/footer.php'; ?>
+
