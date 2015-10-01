03b2d8a47cedfaf6c9a581c32a35b2d41f07f92e
sbot <sbot.opensipa@gmail.com>
2015-10-02 00:00:40 +0200

    Change structure DB -> utenti_message . Apply the patch 1_update.sql only not first installation. Or installation is antecedent at 01/10/2015

--- sql/1_update.sql
+++ sql/1_update.sql
@@ -3,7 +3,7 @@
 -- Questo aggiornamento è da applicare solo se avete fatto una installazione
 -- di Sbot prima del 01 ottobre 2015, altrimenti basta usare telegram_base.sql
 --
--- Apply the patch 1_update.sql only before not installation.
+-- Apply the patch 1_update.sql only not first installation. Or installation is antecedent at 01/10/2015
 
 ALTER TABLE `utenti_message` ADD `ID` INT(11) NOT NULL FIRST;
 UPDATE `utenti_message` SET `ID` = `Message`;
