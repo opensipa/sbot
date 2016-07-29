7b1282ec43603556b493e239ec123d5607cd421b
guia78 <matteo.guion@gmail.com>
2016-07-29 19:22:03 +0200

    New version 0.51.3 -

--- functions/functionPlugin.php
+++ functions/functionPlugin.php
@@ -57,27 +57,25 @@ $nodo = $xmldom->getElementsByTagName("item");
 $risultato = "";
 $conteggio = 0;
 // Scorre tutti i nodi <item> della pagina
-// Limita a 7 il blocco di estrazione
+// Limit to 20 the extract node
 for($i=0; $i<=$nodo->length-1; $i++){
     $conteggio = $conteggio + 1;
-    if ($conteggio>14){
-        break;
+    if ($conteggio>20){
+    break;
     } else {
         // Estraggo il contenuto dei singoli tag del nodo <item>
-        $categoria = $nodo->item($i)->getElementsByTagName("category")->item(0)->childNodes->item(0)->nodeValue;
-            if (is_null($categoria)){
-                $titolo = $nodo->item($i)->getElementsByTagName("title")->item(0)->childNodes->item(0)->nodeValue;
-                $collegamento = $nodo->item($i)->getElementsByTagName("link")->item(0)->childNodes->item(0)->nodeValue;
-                //Short link create
-                $shortURL = initShort($collegamento);
-                // Not used:
-                // $descrizione = $nodo->item($i)->getElementsByTagName("description")->item(0)->childNodes->item(0)->nodeValue;
-                $risultato .= $titolo."\r\n".$shortURL."\r\n\r\n";
-            } else {
-                $risultato .= $categoria;
-            }
-        }
-    } 
+        $titolo = $nodo->item($i)->getElementsByTagName("title")->item(0)->childNodes->item(0)->nodeValue;
+        if ($titolo != ""){
+        $collegamento = $nodo->item($i)->getElementsByTagName("link")->item(0)->childNodes->item(0)->nodeValue;
+        //Short link create
+        $shortURL = initShort($collegamento);
+        // Not used:
+        // $descrizione = $nodo->item($i)->getElementsByTagName("description")->item(0)->childNodes->item(0)->nodeValue;
+        $risultato .= $titolo."\r\n".$shortURL."\r\n\r\n";
+    } else {
+        $risultato = "Non ci sono feed rss per oggi.";}
+    }
+}  
 return $risultato;
 }
 
