52b7d4f9cd20815c2965786f28d94038aeb97551
sbot <sbot.opensipa@gmail.com>
2016-10-06 14:26:44 +0200

    New version 0.51.5 BugFix

--- coda.php
+++ coda.php
@@ -8,23 +8,23 @@ include('functions/functionInit.php');
 ?>
 
 <div id="content" class="clearfix">
-	<div class="content-row">
-            <h2>
-            <?php header('Refresh: 20');
-            // inizializzo cURL
-            $output = controlTelgramState();
-            $risultato = $output[0];
-            $controllo = $output[1];
-            if( $risultato == $controllo ){
-                echo '<br>Non ci sono messaggi in coda. Il sistema funziona correttamente.</h2>'; 
-            } else {
-                echo'<br>Il sistema non st&agrave funzionando correttamente (controlla il demone).</h2><br>'
-                .   'Hai questi messaggi in coda: <br><br>';
-                echo $risultato;  
-            }
-            ?>			
-            
-	</div>			
+    <div class="content-row">
+        <h2>
+        <?php header('Refresh: 20');
+        // inizializzo cURL
+        $output = controlTelgramState();
+        $risultato = $output[0];
+        $controllo = $output[1];
+        if( $risultato == $controllo ){
+            echo '<br>Non ci sono messaggi in coda. Il sistema funziona correttamente.</h2>'; 
+        } else {
+            echo'<br>Il sistema non st&agrave funzionando correttamente (controlla il demone).</h2><br>'
+            .   'Hai questi messaggi in coda: <br><br>'
+            .   $risultato;  
+        }
+        ?>			
+
+    </div>			
 </div>
 <!-- Footer page -->
 <?php include ('theme/footer.php');?>
